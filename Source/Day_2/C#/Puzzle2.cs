fb60c345ac7d6fb18405169cd32ad2c780d31244
Jorge Baptista <jorge-baptista@outlook.com>
2018-12-12 17:37:31 +0000

    initiate problem 2 day 2

---
+++ Source/Day_2/C#/Puzzle2.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Day_2
+{
+    class Puzzle2
+    {
+        public static int Solve(string[] args)
+        {
+            return 0;
+        }
+    }
+}
