367122723a5f26b00872c1677ed650c04953d652
Jorge Baptista <jorge-baptista@outlook.com>
2018-12-29 17:18:42 +0000

    Optimized Python code, updated Readme.md files

--- source/2018/day_01/Python/main.py
+++ source/2018/day_01/Python/main.py
@@ -1,32 +1,32 @@
 with open("../input.txt", "r") as information:
-    inputs = information.readlines()
-
+    input = information.readlines()
 
 def solve_puzzle1():
     result = 0
     
-    for data_input in inputs:
-        result += int(data_input)
+    for data in input:
+        result += int(data)
     
     return result
         
 
 def solve_puzzle2():
     result = 0
-    frequencies = []
+    unique_frequencies = set()
+    data = list(map(int, input))
 
     while True:
-        for data_input in inputs:
-            result += int(data_input)
+        for i in data:
+            result += i
 
-            if result in frequencies:
+            if result in unique_frequencies:
                 return result
 
-            frequencies.append(result)
+            unique_frequencies.add(result)
 
 
 puzzle1_solution = solve_puzzle1()
 puzzle2_solution = solve_puzzle2()
 
 print("Puzzle 1 Solution :" , puzzle1_solution)
-print("Puzzle 2 Solution :" + puzzle2_solution)
+print("Puzzle 2 Solution :" , puzzle2_solution)
--- source/2018/day_01/Python/puzzle_01.py
+++
@@ -1,7 +0,0 @@
-# class Puzzle1:
-#     def Solve(inputs):
-#         result = 0
-#         for dataInput in inputs:
-#             result += int(input)
-
-#         return result
